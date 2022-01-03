e2f54c14c704724176d6a5defe89cdc8e62a3053
LilyaSari <92746373+LilyaSari@users.noreply.github.com>
2022-01-03 20:27:02 +0100

    add subs to detailbook

--- resources/views/detailbook.blade.php
+++ resources/views/detailbook.blade.php
@@ -448,6 +448,76 @@
                                 <h6><b>Category</b> : Horror</h6>
                                 <h6><b>Keyword</b> : Thriller , True story , American</h6>
                             </div>
+                            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        <h6>Subscribers of this book</h6>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        First name
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Last name
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Birthday
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        ID
+                                    </th>
+                                    <th class="text-secondary opacity-7"></th>
+                                    <th class="text-secondary opacity-7"></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                
+                                    <tr class="survol" onclick="location.href='profile.html'"
+                                        style="cursor:pointer;">
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-sm"></h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs font-weight-bold mb-0"></p>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <p class="text-xs font-weight-bold mb-0"></p>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <p class="text-xs font-weight-bold mb-0"></p>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="EditSubscriber.html"
+                                               class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
+                                               data-original-title="Edit user">
+                                                Edit
+                                            </a>
+                                        </td>
+                                        <td class="align-middle">
+                                            <form class="row g-3">
+                                                <div class="col-md-12">
+                                                    @csrf
+                                                    <button type="" class="btn btn-danger" style="color: #fff">
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </td>
+                                    </tr>
+                              
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
                         </div>
                     </div>
                 </div>
